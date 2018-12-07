e3bc07253c1efec50af4e539abbcf2a19fe60bf4
Eliran Eliassy <eliran.eliassy@gmail.com>
2018-12-07 16:05:54 +0200

    fix ui, add bootstrap

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../item.interface';
 
 @Component({
   selector: 'app-item',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  @Input() item: any;
+  @Input() item: Item;
 
   constructor() { }
 
